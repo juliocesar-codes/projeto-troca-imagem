058f1b108206d2ca3ea8beab79b873c6bf43f8fe
JULIO CESAR SANTANA ALVES <25132583@dsn127m1241953.cfp127aluno.local>
2025-08-11 14:17:33 -0300

    Automatização funcionando, requista algumas atualizações

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 'use stratic'
 
 const botaoTrocarCidade = document.getElementById('trocar-cidade')
+const botaoAuto = document.getElementById('auto')
 
 function trocarCidade() {
     const cidade = document.getElementById('cidade').value
-    document.documentElement.style.setProperty('--bg-img', `url('./img/${cidade}.webp')`)
+    document.documentElement.style.setProperty('--bg-img', `url('./img/${cidade}.jpg')`)
+}
+
+function trocarCidadeAuto() {
+    const cidades = [
+        'url(./img/barueri.jpg)',
+        'url(./img/cotia.jpg)',
+        'url(./img/itapevi.jpg)',
+        'url(./img/jandira.jpg)',
+        'url(./img/osasco.jpg)'
+    ]
+
+    let posicao = 0
+
+    function slide() {
+        document.documentElement.style.setProperty('--bg-img', cidades[posicao])
+        posicao = (posicao + 1) % cidades.length
+    }
+
+    slide()
+    setInterval(slide, 3000)
+
 }
 
 botaoTrocarCidade.addEventListener('click', trocarCidade)
+botaoAuto.addEventListener('click', trocarCidadeAuto)
